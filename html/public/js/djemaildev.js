f6d1bd2891afa625a480ccd2b08d1541962d2d48
Unknown <gilmorej@otc.edu>
2019-11-25 21:31:01 -0600

    DJDoDev Updates

--- html/public/js/djemaildev.js
+++ html/public/js/djemaildev.js
@@ -40,10 +40,6 @@ const sendMail = (email, name, message) => {
                 console.error(err, err.stack);
                 submitForm();
             });
-    // AWS.SES.sendEmail(params, function (err, data) {
-    //     if (err) console.log(err, err.stack);
-    //     else console.log(data);
-    // });
 }
 
 module.exports = sendMail;
