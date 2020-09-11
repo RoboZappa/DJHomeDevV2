485f70659d4ca01ef69c4ac0d4554641513f156c
Unknown <gilmorej@otc.edu>
2020-09-10 20:07:53 -0500

    HTTPS Redirect WIP (HTTPS works)

--- html/server.js
+++ html/server.js
@@ -1,40 +1,61 @@
 const express = require('express');
 const http = require('http');
 const https = require('https');
-// const forceSsl = require('express-force-ssl');
 const fs = require('fs');
 const sendMail = require('./public/js/djemaildev');
 const app = express();
-// const options = {
-//     key: fs.readFileSync('./creds/key.pem', 'utf8'),
-//     cert: fs.readFileSync('./creds/server.crt', 'utf8')
-// }
-// const options = {
-//     key: fs.readFileSync('/etc/letsencrypt/live/djwebdev.net/privkey.pem', 'utf8'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/djwebdev.net/cert.pem', 'utf8')
-// }
+const httpPort = 8000;
+const httpsPort = 443;
+
 app.use(express.urlencoded({
     extended: false
 }));
 app.use(express.json());
 
+// Production Creds
+//const options = {
+    // key: fs.readFileSync('/etc/letsencrypt/live/djwebdev.net/privkey.pem', 'utf8' ),
+    // cert: fs.readFileSync('/etc/letsencrypt/live/djwebdev.net/cert.pem', 'utf8')
+//}
+
+// Local Creds
+const options = {
+     key: fs.readFileSync('./creds/key.pem', 'utf8' ),
+     cert: fs.readFileSync('./creds/server.crt', 'utf8')
+}
+
+// Send Email method from djemaildev.js
 app.post('/email', (req, res) => {
     const { email, name, message } = req.body;
     console.log('Data: ', req.body);
-    
-    sendMail(email, name, message, function(err, data) {
+
+    sendMail(email, name, message, function (err, data) {
         if (err) {
             console.log('ERROR: ', err);
-            return res.status(500).json({message: err.message || 'Internal Error'});
-        } 
+            return res.status(500).json({ message: err.message || 'Internal Error' });
+        }
         console.log('Email Submitted');
-        return res.json({ message: 'Email Submitted'});
+        return res.json({ message: 'Email Submitted' });
     });
-})
+});
 
+// http.get('*', function(req, res) {
+//     if (req.protocol === 'http') {
+//         res.redirect('https://djwebdev.net');
+//     }
+// })
+
+app.use(function (req, res, next) {
+    console.log('Something: ', req.get('Host'));
+    if (!req.secure) {
+        console.log(req.get('Host'));
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
+});
 app.use('/', express.static('public'));
 
-http.createServer(app).listen(8000);
-// console.log('Express http server running on port 4000');
-// https.createServer(options, app).listen(8000);
-console.log('Express https server running on port 8000');
+http.createServer(app).listen(httpPort);
+console.log('Express server running on http port: ', httpPort);
+https.createServer(options, app).listen(httpsPort);
+console.log('Express server running on https port: ', httpsPort);
