1562aa71c367c697345e8f45b8991712dd67c8a4
Dave <evadssor@gmail.com>
2022-07-03 23:04:20 -0500

    modal improvements
    changes to the modal's appearance and placement

--- html/public/DJWebDevV3/js/main.js
+++ html/public/DJWebDevV3/js/main.js
@@ -1,69 +1,35 @@
 Vue.component("email-modal", {
-  props: ["email", "name", "message"],
-  template: `<div class="modal-background">
+  template: 
+  `<div class="modal-background">
     <div class="modal">
-      <div id="contact">
-        <div id="formModal">
-          <div class="social">
-            <ul>
-              <li>
-                <a
-                  href="http://www.facebook.com/officialdjwebdev"
-                  target="_blank"
-                  >Facebook</a
-                >
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/officialdjwebdev"
-                  target="_blank"
-                  >Instagram</a
-                >
-              </li>
-            </ul>
-          </div>
-          <div class="container">
-            <div id="form">
-              <form id="contactForm">
-                <label for="name">Name:</label>
-                <input
-                  class="u-full-width"
-                  type="text"
-                  id="name"
-                  placeholder="Bob Sacamano"
-                />
-                <label for="email">Email:</label>
-                <input
-                  class="u-full-width"
-                  type="text"
-                  id="email"
-                  placeholder="bob@gmail.com"
-                />
-                <label for="message">Message:</label>
-                <textarea
-                  class="u-full-width"
-                  placeholder="Message..."
-                  id="message"
-                ></textarea>
-                <div class="formButtons">
-                  <button
-                    type="submit"
-                    value="Submit"
-                    onclick="submitForm(email)"
-                  >
-                    SUBMIT
-                  </button>
-                  <button type="button" id="close" @click="modal = false">
-                    CLOSE
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
+      <div class="social">
+        <a href="http://www.facebook.com/officialdjwebdev"target="_blank">Facebook</a>
+        <a href="https://www.instagram.com/officialdjwebdev" target="_blank">Instagram</a>
+      </div>
+      <div class="contactForm">
+        <div class="formName">
+          <label for="name">Name:</label><br>
+          <input class="u-full-width" type="text" id="name" placeholder="Bob Sacamano"/>  
         </div>
+        <div class="formEmail">
+          <label for="email">Email:</label><br>
+          <input class="u-full-width" type="text" id="email" placeholder="bob@gmail.com"/>
+        </div>
+        <div class="formMessage">
+          <label for="message">Message:</label><br>
+          <textarea class="u-full-width" placeholder="Message..." id="message"></textarea>
+        </div>
+        <div class="formButtons">
+          <button type="submit" value="Submit" onclick="submitForm(email)">SUBMIT</button>
+          <button v-bind:modal="false" v-on:input="modal=$event.target.value" type="button" id="close">CLOSE</button>
+        </div>
+      </div>
+      <div class="modalLogo">
+        <h3>_DJ<br>Web<br>Dev</h3>
       </div>
     </div>
   </div>`
+  ,props:["modal"]
 });
 
 
